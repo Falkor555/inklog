606fcf465a1ca332dec05dcd06df98e18e735650
Falkor555 <loic.botsy@hotmail.com>
2026-04-23 09:03:56 +0400

    initialisation de notre projet Java Spring Inklog

---
+++ src/main/java/com/cocacola/inklog/InklogApplication.java
@@ -0,0 +1,13 @@
+package com.cocacola.inklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InklogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cocacola/inklog/InklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocacola.inklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
